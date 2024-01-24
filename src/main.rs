18e2d6938c55656ee5aa1220e6debe0611aa26e3
Lamafoos <patrik.moquist@gmail.com>
2024-01-24 16:31:13 +0100

    added test book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use epub::doc::EpubDoc;
+
 fn main() {
-    println!("Hello, world!");
+    let doc = EpubDoc::new("./epubs/wallace-sandi-the-kingmaker.epub");
+    assert!(doc.is_ok());
+
+    let doc = doc.unwrap();
+    let meta = &doc.metadata;
+
+    for (key, value) in meta {
+        println!("{:?}: \"{:?}\"", key, value);
+    }
 }
