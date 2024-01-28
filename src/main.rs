b71449f3076f79442d744a8ca9e734eff90364e3
Lamafoos <patrik.moquist@gmail.com>
2024-01-28 18:19:32 +0100

    meta-test

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,14 @@ use rp_pico::entry;
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let doc = EpubDoc::new("./epubs/wallace-sandi-the-kingmaker.epub");
+
+    if doc.is_ok() {
+        let doc = doc.unwrap();
+        let meta = &doc.metadata;
+
+        for (key, value) in meta {
+            println!("{:?}: {:?}", key, value);
+        }
+    }
 }
